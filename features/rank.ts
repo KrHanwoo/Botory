ac0410ac9503f7d3173779bde21dfee8222caaac
KrHanwoo <koreahan8549@gmail.com>
2023-11-26 15:48:30 +0900

    Change role limit

--- features/rank.ts
+++ features/rank.ts
@@ -5,6 +5,8 @@ import { Money } from "../utils/money";
 import { Bot } from "../bot";
 import { Document, WithId } from "mongodb";
 import { Xp } from "../utils/xp";
+import { Collection, Message } from "discord.js";
+import { Canvas } from "canvas";
 
 export class Rank {
 
@@ -15,39 +17,51 @@ export class Rank {
 }
 
 async function updateRank() {
+  let messages = await BotCache.rank.messages.fetch({ limit: 5 });
   let moneyRankings = await Money.getRankings();
   let xpRankings = await Xp.getRankings();
 
-  await processRoles(Bot.config.role.rich, moneyRankings);
-  await processRoles(Bot.config.role.dichang, xpRankings);
-
   let moneyCanvas = await RankFrame.createMoneyTable(moneyRankings);
   let xpCanvas = await RankFrame.createRankTable(xpRankings);
-  let moneyData = { files: [{ attachment: moneyCanvas.toBuffer(), name: `money_rank_${Util.currentDate()}.png` }] };
-  let xpData = { files: [{ attachment: xpCanvas.toBuffer(), name: `money_rank_${Util.currentDate()}.png` }] };
+  processTable(moneyCanvas, 'money', messages);
+  processTable(xpCanvas, 'xp', messages);
 
-  let messages = await BotCache.rank.messages.fetch({ limit: 5 });
-  let moneyMsg = messages.filter(x => x.attachments.some(a => a.name.startsWith('money_rank_'))).at(0);
-  let xpMsg = messages.filter(x => x.attachments.some(a => a.name.startsWith('xp_rank_'))).at(0);
-
-  if (!moneyMsg || !moneyMsg.editable) BotCache.rank.send(moneyData);
-  else moneyMsg.edit(moneyData);
-  if (!xpMsg || !xpMsg.editable) BotCache.rank.send(xpData);
-  else xpMsg.edit(xpData);
+  await processRoles(Bot.config.role.dichang, xpRankings, 3);
+  await processRoles(Bot.config.role.rich, moneyRankings, 5, Bot.config.role.dichang);
 }
 
-async function processRoles(id: string, rankings: WithId<Document>[]) {
+async function processTable(table: Canvas, id: string, messages: Collection<string, Message>) {
+  let data = { files: [{ attachment: table.toBuffer(), name: `${id}_rank_${Util.currentDate()}.png` }] };
+  let msg = messages.filter(x => x.attachments.some(a => a.name.startsWith(`${id}_rank_`))).at(0);
+  if (!msg || !msg.editable) BotCache.rank.send(data);
+  else msg.edit(data);
+}
+
+async function processRoles(id: string, rankings: WithId<Document>[], limit: number, exclude?: string) {
   const role = await BotCache.guild.roles.fetch(id);
-  if (role) {
-    let eligible = rankings.slice(0, 5);
-    let roleRemove = role.members.filter(m => !eligible.some(x => x.member == m.id));
-    let roleAdd = eligible.filter(r => !role.members.some(x => x.id == r.member));
-    for (let pair of roleRemove) {
-      await pair[1].roles.remove(id).catch(() => null);
+  let excludeRole;
+  if (exclude) excludeRole = await BotCache.guild.roles.fetch(exclude);
+  if (!role) return;
+  let idx = -1;
+  let eligible: string[] = [];
+  for (let i = 0; i < limit; i++) {
+    idx++;
+    if (idx + 1 > rankings.length) return;
+    let memberId = rankings[idx].member;
+    if (excludeRole && excludeRole.members.some(x => x.id == memberId)) {
+      i--;
+      continue;
     }
-    for (let doc of roleAdd) {
-      let member = await BotCache.guild.members.fetch(doc.member);
-      await member.roles.add(id).catch(() => null);
-    }
+    eligible.push(memberId);
+  }
+
+  let roleRemove = role.members.filter(m => !eligible.some(x => x == m.id));
+  let roleAdd = eligible.filter(r => !role.members.some(x => x.id == r));
+  for (let pair of roleRemove) {
+    await pair[1].roles.remove(id).catch(() => null);
+  }
+  for (let mId of roleAdd) {
+    let member = await BotCache.guild.members.fetch(mId);
+    await member.roles.add(id).catch(() => null);
   }
 }
